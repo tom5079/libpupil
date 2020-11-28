e738ba8227e7b31bf13f1a174529df58f2ab584b
tom5079 <tom5079@naver.com>
2020-11-28 16:14:13 +0900

    1.9.4

--- src/main/kotlin/xyz/quaver/hiyobi/search.kt
+++ src/main/kotlin/xyz/quaver/hiyobi/search.kt
@@ -5,13 +5,14 @@ import okhttp3.MediaType
 import okhttp3.Request
 import okhttp3.RequestBody
 import xyz.quaver.client
+import xyz.quaver.json
 import java.io.IOException
 
 private const val PER_PAGE = 15
-fun search(query: String, range: IntRange): Pair<List<String>, Int> {
+fun search(query: String, range: IntRange): Pair<List<GalleryBlock>, Int> {
     val url = "https://api.hiyobi.me/search"
 
-    val result = mutableListOf<String>()
+    val result = mutableListOf<GalleryBlock>()
 
     val requests = (range.first .. range.last step PER_PAGE).map { JsonObject(mapOf(
         "search" to JsonArray(query.split(" ").map { JsonPrimitive(it) }),
@@ -47,7 +48,45 @@ fun search(query: String, range: IntRange): Pair<List<String>, Int> {
         }
 
         result.addAll(
-            (respose.jsonObject["list"]?.jsonArray?.mapNotNull { it.jsonObject["id"]?.jsonPrimitive?.content } ?: emptyList())
+            (respose.jsonObject["list"]?.jsonArray?.mapNotNull { json.decodeFromJsonElement<GalleryBlock>(it) } ?: emptyList())
+                .slice(slice)
+        )
+    }
+
+    return Pair(result, count)
+}
+
+fun list(range: IntRange): Pair<List<GalleryBlock>, Int> {
+    val url = "https://api.hiyobi.me/list/"
+
+    val result = mutableListOf<GalleryBlock>()
+
+    val pages = (range.first / PER_PAGE)+1 .. (range.last / PER_PAGE)+1
+
+    var count = -1
+
+    pages.forEachIndexed { index, page ->
+        val text = client.newCall(
+            Request.Builder()
+                .url(url + page.toString())
+                .build()
+        ).execute().also { if (it.code() != 200) throw IOException() }.body()?.use { it.string() } ?: throw IOException()
+
+        val respose = Json.parseToJsonElement(text)
+
+        if (count < 0)
+            count = respose.jsonObject["count"]?.jsonPrimitive?.int ?: -1
+
+        val slice = when {
+            index != 0 && index != range.last / PER_PAGE -> 0 until PER_PAGE
+            index == 0 && index == range.last / PER_PAGE -> range.first .. range.last
+            index == 0 -> range.first until PER_PAGE
+            index == range.last / PER_PAGE -> 0 .. range.last % PER_PAGE
+            else -> error("")
+        }
+
+        result.addAll(
+            (respose.jsonObject["list"]?.jsonArray?.mapNotNull { json.decodeFromJsonElement<GalleryBlock>(it) } ?: emptyList())
                 .slice(slice)
         )
     }
